fb70dac62a88b5fca49eb92b017791fb9c938f96
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-10 23:46:09 +0530

    logo in navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,12 +1,22 @@
-import React, { use } from 'react'
+import React, { use, useState } from 'react'
+import OverlayMenu from './OverlayMenu'
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
 const [menuopen,setMenuOpen]=useState(false)
+const [visible,setVisible]=useState(true)
 
   return (
-    <nav>
-      Navbar
+    <>
+    <nav className ={ `fixed top-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${ visible ? "translate-y-0" :"-translate-y-full"}`}>
+
+    <div>
+        <img src={logo} alt="" />
+    </div>
+
     </nav>
+    <OverlayMenu/>
+    </>
   )
 }
 
