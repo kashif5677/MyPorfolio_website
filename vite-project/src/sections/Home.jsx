fedab52669c1d9099a786c63158ddf3847e39e97
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-13 22:41:26 +0530

    impulse animation

--- vite-project/src/sections/Home.jsx
+++ vite-project/src/sections/Home.jsx
@@ -17,8 +17,26 @@ const Home = () => {
       animate-pulse
       '> 
       </div>
-      <div></div>
+      <div  className='absolute bottom-0 right-0 
+      w-[70vw]  sm:w-[50vw]  md:w-[40vw]
+      h-[70vw]  sm:h-[50vw]  md:h-[40vw]
+      max-w-[400px] max-h-[400px]
+      rounded-full
+      bg-gradient-to-r from-[#302b63] via-[#00bf8f] to-[#1cd8d2]
+      opacity-50 sm:opacity-20 md:opacity-10
+      blur-[100px] sm:blur-[150px] md:blur-[200px]
+      animate-pulse delay-500
+      '></div>
       </div>
+
+    <div className='relative z-10 h-full w-full max-w-7xl mx-auto grid-cols-1 lg:grid-cols-2 '>
+    <div className='flex flex-col justify-center h-full text-center lg:text-left relative '>
+      <div className='w-full lg:pr-24 mx-auto max-w-[48rem}'></div>
+    </div>
+    </div>jhuhjijktrfewfy
+
+
+
     </section>
   )
 }
