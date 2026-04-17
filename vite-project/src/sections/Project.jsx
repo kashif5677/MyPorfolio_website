a68b0aa2bc6ee411c4012e9e88fbb55117032f6f
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-17 23:52:14 +0530

    scroll

--- vite-project/src/sections/Project.jsx
+++ vite-project/src/sections/Project.jsx
@@ -1,20 +1,82 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState,useRef, useScroll } from 'react'
+import {scrollYProgress} from useScroll
+import photo1 from "../assets/photo1.jpg"
+import photo2 from "../assets/photo1.jpg"
+import photo3 from "../assets/photo1.jpg"
+import img1 from "../assets/photo1.jpg"
+import img2 from "../assets/photo1.jpg"
+import img3 from "../assets/photo1.jpg"
+import { useMotionValueEvent } from 'framer-motion'
 
-const useIsMobile={query="(min-width: 768px)"}=>{
+const useIsMobile=(query="(min-width: 768px)")=>{
   const [isMobile, setIsMobile] = useState(
     typeof window !== "undefined" && window.matchMedia(query).matches
   ) 
   useEffect(()=>{
     if(typeof window !== "undefined") return 
     const mql=window.matchMedia(query)
-    const hnadler=(e)=>setIsMobile(e.matches)
-  })
+    const handler=(e)=>setIsMobile(e.matches)
+
+    mql.addListener("change",handler)
+    setIsMobile(mql.matches)
+    return ()=>mql.removeListener("change",handler)
+  },[query])
+  return isMobile
 }
 const Project = () => {
+
+const isMobile=useIsMobile()
+const scenRef=useRef(null)
+
+const projects=useMemo(()=>[
+  {
+  title:"Project",
+  link:"https://github.com",
+  bgColor:"bg-gradient-to-r from-pink-700 to-blue-500",
+  image:isMobile ? photo1 : photo2,
+},
+{
+  title:"Project",
+  link:"https://github.com",
+  bgColor:"bg-gradient-to-r from-pink-700 to-blue-500",
+  image:isMobile ? photo2:photo3,
+},
+{
+  title:"Project",
+  link:"https://github.com",
+  bgColor:"bg-gradient-to-r from-pink-700 to-blue-500",
+  image:isMobile ? img1 : img2,
+}
+
+],[isMobile])
+
+const {scrollYProgress}= useScroll({
+  target:scenRef,
+  offset:["start start","end end"]
+})
+
+const threshold=projects.map((_,i)=>(i+1)/projects.length)
+const [activeIndex,setActiveIndex]=useState(0)
+
+useMotionValueEvent(scrollYProgress,"change",(v)=>{
+  const idx=threshold.findIndex((t)=>v<t)
+  setActiveIndex(idx === -1 ? threshold.length - 1 : idx)
+})
+
+const activeProject=projects[activeIndex]
+
   return (
-    <div id='projects' className='relative text-white'>
+    <section 
+    ref={scenRef}
+    id='projects' 
+    className='relative text-white'
+    style={{
+      height:`${100*projects.length}vh`,
+      backgroundColor:activeProject.bgColor
+    }}
+    >
       
-    </div>
+    </section>
   )
 }
 
